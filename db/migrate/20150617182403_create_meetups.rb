a2dc4c7e4b1704121d1eb0a604de935d1c3b165a
Brian Stern <briansternhk@gmail.com>
2015-06-17 23:24:29 -0400

    migration change

--- db/migrate/20150617182403_create_meetups.rb
+++ db/migrate/20150617182403_create_meetups.rb
@@ -1,7 +1,7 @@
 class CreateMeetups < ActiveRecord::Migration
   def change
     create_table :meetups do |t|
-      t.string :name, null: false
+      t.string :name, unique: true, null: false
       t.string :location, null: false
       t.text :description, null: false
     end
